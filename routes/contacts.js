5755bd94b60df41d8a59d649796349165fa58df1
Rafael Armenta <rarmenta@hawk.iit.edu>
2023-02-19 14:53:35 -0600

    changed contacts pug
    added somethings to contacts pug and set some default text

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let data = [
+  {text: 'This is contact 1 text', id: 'a593c7cf-889d-4e81-bf0a-76cfd2d9810d'},
+  {text: 'This is contact 2 text', id: '7054c36e-d85e-41d3-8fd8-a996e0cac276'}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with contacts');
+  res.render('contacts', {title: 'Express Contacts', contacts: data });
 });
 
 module.exports = router;

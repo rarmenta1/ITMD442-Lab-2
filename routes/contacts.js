9967974b896e36a97cefe4d37771a561b57aa5d2
Rafael Armenta <rarmenta@hawk.iit.edu>
2023-02-20 08:28:00 -0600

    data is now saved to a data file

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const contactsRepo = require('../src/contactsMemoryRepository');
+//const contactsRepo = require('../src/contactsMemoryRepository');
+const contactsRepo = require('../src/contactsFileRepository');
 
 
 
